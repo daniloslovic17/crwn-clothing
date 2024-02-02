a4b095f6c267a90fa1c49b26262e86ba489da249
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-02 17:24:33 +0100

    Finish Creating User Document

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -30,8 +30,27 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef);
-
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot.exists()); 
+
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch(error){
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userDocRef;
+
+    // if user data does not exist
+    // create / set the document with the data from userAuth in my collection
+
+    // if user data exists
+    // return userDocRef
 };
