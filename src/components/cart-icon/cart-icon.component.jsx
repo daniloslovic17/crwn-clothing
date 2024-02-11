1d98389bacada22b3727e6c797620e4b723ded4f
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-11 21:41:42 +0100

    Add to Cart 2

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 import { useContext } from 'react';
-import { CartContext } from '../../context/cart.context';
+import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen } = useContext(CartContext);
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,9 +4,9 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { UserContext } from "../../context/user.context";
+import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import { CartContext } from "../../context/cart.context";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
