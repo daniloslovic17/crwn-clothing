1fdbbd1603c6bb77a40d6d15491c7a3563196e28
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-12 18:13:27 +0100

    Cart Count their way

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,18 +4,14 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen, cartItems } = useContext(CartContext);
-
-    const totalCartCount = cartItems.reduce((acc, item) => {
-        return acc + item.quantity;
-    }, 0 )
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
             <span className='item-count'>
-                {totalCartCount}
+                {cartCount}
             </span>
         </div>
     );
