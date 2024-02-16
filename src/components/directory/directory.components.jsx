2bd33c75a11565df039733bc06fbc325d64f9c13
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-16 17:20:12 +0100

    Fixing Clashins styles 2

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -3,7 +3,7 @@ import './directory.styles.scss';
 
 const Directory = ({categories}) => {
   return(
-    <div className="categories-container">
+    <div className="directory-container">
       {categories.map( ({ title, id, imageUrl }) => (
         <DirectoryItem key={id} title={title} imageUrl={imageUrl} />
       ) )}
