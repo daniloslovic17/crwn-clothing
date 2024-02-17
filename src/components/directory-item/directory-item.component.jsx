c08d27572bb8aace1b0130878c12b5aa353ba821
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-17 12:37:09 +0100

    Last Touches

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,19 @@
-import './directory-item.styles'
+import { useNavigate } from 'react-router-dom';
 
 import { DirectoryItemContainer, Body, BackgroundImage } from './directory-item.styles';
 
-const DirectoryItem = ({ imageUrl, title }) => {
-    return(
-        <DirectoryItemContainer>
-          <BackgroundImage imageUrl={imageUrl} />
-          <Body>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </Body>
-      </DirectoryItemContainer>
-    );
+const DirectoryItem = ({ imageUrl, title, route }) => {
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+  return(
+      <DirectoryItemContainer onClick={onNavigateHandler}>
+        <BackgroundImage imageurl={imageUrl} />
+        <Body>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Body>
+    </DirectoryItemContainer>
+  );
 }
 
 export default DirectoryItem;
