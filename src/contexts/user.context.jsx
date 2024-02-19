d1fa9dc3611663700c64fe676aebba313d722107
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-19 15:11:14 +0100

    Cart and User Reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -2,6 +2,8 @@ import { createContext, useEffect, useReducer } from "react";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
+import { createAction } from "../utils/reducer/reducer.utils";
+
 // as the actual value you want to access
 export const UserContext = createContext({
     currentUser: null,
@@ -35,7 +37,7 @@ export const UserProvider = ({children}) => {
     const { currentUser } = state;
     
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     }
 
     const value = { currentUser, setCurrentUser };
