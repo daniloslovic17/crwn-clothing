e34cd104da46033c71f9e0706aae02a03022802e
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-28 19:00:07 +0100

    process.env.NODE_ENV

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
