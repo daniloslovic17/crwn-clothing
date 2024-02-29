12cd29dcb91d87eb465bfca23019cab5fe6f62b7
daniloslovic17 <daniloslovic17@gmail.com>
2024-02-29 18:26:30 +0100

    Spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,14 +2,17 @@ import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/categories.selector';
+
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import './category.styles.scss';
-import ProductCard from '../../components/product-card/product-card.component';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -19,11 +22,17 @@ const Category = () => {
     return(
         <Fragment>
             <h2 className='categoty-title'>{category.toUpperCase()}</h2>
-            <div className='category-container'>
-                {
-                    products && products.map((product) => <ProductCard key={product.id} product={product} />)
-                }
-            </div>
+            {
+                isLoading ? (
+                    <Spinner /> 
+                ) : (
+                    <div className='category-container'>
+                        {
+                            products && products.map((product) => <ProductCard key={product.id} product={product} />)
+                        }
+                    </div>
+                )
+            }
         </Fragment>
     );
 };
