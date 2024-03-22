4a7101260c4c745df782b10ae3fa7c99be38bfdb
daniloslovic17 <daniloslovic17@gmail.com>
2024-03-22 12:02:54 +0100

    createAction and function overloading

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,1 +1,18 @@
-export const createAction = (type, payload) => ({ type, payload });
+import { AnyAction } from "redux"; 
+
+export type ActionWithPayload<T, P> = {
+    type: T;
+    payload: P;
+}
+
+export type Action<T> = {
+    type: T;
+}
+
+export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
+
+export function createAction<T extends string>(type: T, payload: void): Action<T>;
+
+export function createAction<T extends string, P>(type: T, payload: P){
+    return{ type, payload };
+}
