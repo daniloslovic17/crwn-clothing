7eb65f16960c740bd2c5db43417b36ecc05485d5
daniloslovic17 <daniloslovic17@gmail.com>
2024-04-01 21:12:46 +0200

    Typing Category Action Creators

--- src/store/categories/categories.types.ts
+++ src/store/categories/categories.types.ts
@@ -6,3 +6,16 @@ export enum CATEGORIES_ACTION_TYPES {
 };
 
 export default CATEGORIES_ACTION_TYPES;
+
+export type CategoryItem = {
+    id: number,
+    imageUrl: string,
+    name: string,
+    price: number;
+}
+
+export type Category = {
+    title: string;
+    imageUrl: string,
+    items: CategoryItem[]
+}
